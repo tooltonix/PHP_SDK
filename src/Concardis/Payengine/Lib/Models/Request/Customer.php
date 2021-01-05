9777c439cd1cbfedb246139674a4726abd63c74b
Patrick Taddey <patrick.taddey@buenting.de>
2021-01-05 13:20:14 +0100

    making properties companyName, customerType and organizationVatId nullable

--- src/Concardis/Payengine/Lib/Models/Request/Customer.php
+++ src/Concardis/Payengine/Lib/Models/Request/Customer.php
@@ -24,28 +24,28 @@ class Customer extends AbstractModel
     /**
      * required for b2b: official name of the organization
      *
-     * @var string
+     * @var string|null
      */
     private $companyName;
 
     /**
      * required for b2b: when customer is an organization, this parameter has one possible value: 'organization'
      *
-     * @var string
+     * @var string|null
      */
     private $customerType;
 
     /**
      * optional for b2b: unique VAT ID number of the organization
      *
-     * @var string
+     * @var string|null
      */
     private $organizationVatId;
 
     /**
      * @return string
      */
-    public function getEmail()
+    public function getEmail(): string
     {
         return $this->email;
     }
@@ -53,7 +53,7 @@ class Customer extends AbstractModel
     /**
      * @param string $email
      */
-    public function setEmail($email)
+    public function setEmail(string $email): void
     {
         $this->email = $email;
     }
@@ -61,7 +61,7 @@ class Customer extends AbstractModel
     /**
      * @return string
      */
-    public function getMerchantCustomerId()
+    public function getMerchantCustomerId(): string
     {
         return $this->merchantCustomerId;
     }
@@ -69,55 +69,55 @@ class Customer extends AbstractModel
     /**
      * @param string $merchantCustomerId
      */
-    public function setMerchantCustomerId($merchantCustomerId)
+    public function setMerchantCustomerId(string $merchantCustomerId): void
     {
         $this->merchantCustomerId = $merchantCustomerId;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getCompanyName(): string
+    public function getCompanyName(): ?string
     {
         return $this->companyName;
     }
 
     /**
-     * @param string $companyName
+     * @param string|null $companyName
      */
-    public function setCompanyName(string $companyName): void
+    public function setCompanyName(?string $companyName): void
     {
         $this->companyName = $companyName;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getCustomerType(): string
+    public function getCustomerType(): ?string
     {
         return $this->customerType;
     }
 
     /**
-     * @param string $customerType
+     * @param string|null $customerType
      */
-    public function setCustomerType(string $customerType): void
+    public function setCustomerType(?string $customerType): void
     {
         $this->customerType = $customerType;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getOrganizationVatId(): string
+    public function getOrganizationVatId(): ?string
     {
         return $this->organizationVatId;
     }
 
     /**
-     * @param string $organizationVatId
+     * @param string|null $organizationVatId
      */
-    public function setOrganizationVatId(string $organizationVatId): void
+    public function setOrganizationVatId(?string $organizationVatId): void
     {
         $this->organizationVatId = $organizationVatId;
     }
