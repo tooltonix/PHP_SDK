b295232e2644d75ee2885a7541883eec919cc9cf
Patrick Taddey <tooltonix@gmail.com>
2021-01-07 19:54:08 +0100

    Add B2B support for Ratepay (#2)
    * added new fields to the customer class
    
    * new field organizationVatId added
    
    * making properties companyName, customerType and organizationVatId nullable
    
    * Added Constants for 3DS Authentication Status
    
    Co-authored-by: Patrick Taddey <patrick.taddey@buenting.de>

--- src/Concardis/Payengine/Lib/Models/Request/Customer.php
+++ src/Concardis/Payengine/Lib/Models/Request/Customer.php
@@ -2,22 +2,44 @@
 
 namespace Concardis\Payengine\Lib\Models\Request;
 
-
 use Concardis\Payengine\Lib\Internal\AbstractClass\AbstractModel;
 
 class Customer extends AbstractModel
 {
     /**
      * Email of the customer
+     *
      * @var string
      */
     private $email;
 
     /**
      * Id for this customer on your System
+     *
      * @var string
      */
     private $merchantCustomerId;
+
+    /**
+     * required for b2b: official name of the organization
+     *
+     * @var string|null
+     */
+    private $companyName;
+
+    /**
+     * required for b2b: when customer is an organization, this parameter has one possible value: 'organization'
+     *
+     * @var string|null
+     */
+    private $customerType;
+
+    /**
+     * optional for b2b: unique VAT ID number of the organization
+     *
+     * @var string|null
+     */
+    private $organizationVatId;
 
     /**
      * @return string
@@ -51,4 +73,51 @@ class Customer extends AbstractModel
         $this->merchantCustomerId = $merchantCustomerId;
     }
 
+    /**
+     * @return string|null
+     */
+    public function getCompanyName()
+    {
+        return $this->companyName;
+    }
+
+    /**
+     * @param string|null $companyName
+     */
+    public function setCompanyName($companyName)
+    {
+        $this->companyName = $companyName;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCustomerType()
+    {
+        return $this->customerType;
+    }
+
+    /**
+     * @param string|null $customerType
+     */
+    public function setCustomerType($customerType)
+    {
+        $this->customerType = $customerType;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getOrganizationVatId()
+    {
+        return $this->organizationVatId;
+    }
+
+    /**
+     * @param string|null $organizationVatId
+     */
+    public function setOrganizationVatId($organizationVatId)
+    {
+        $this->organizationVatId = $organizationVatId;
+    }
 }
