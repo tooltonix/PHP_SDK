49b267880a7a26405aeb7faff3ffeffeb58e2da1
Patrick Taddey <patrick.taddey@buenting.de>
2021-03-03 10:45:14 +0100

    CredentialOnFile, extended by $recurringExpiry $recurringFrequency attributes, and extended by corresponding getter/setter

--- src/Concardis/Payengine/Lib/Models/Request/Authentication/CredentialOnFile.php
+++ src/Concardis/Payengine/Lib/Models/Request/Authentication/CredentialOnFile.php
@@ -4,44 +4,94 @@ namespace Concardis\Payengine\Lib\Models\Request\Authentication;
 
 use Concardis\Payengine\Lib\Internal\AbstractClass\AbstractModel;
 
-class CredentialOnFile extends AbstractModel {
+class CredentialOnFile extends AbstractModel
+{
 
-	/**
-	 * @var string Enum:"ONECLICK" "UNSCHEDULED" "SCHEDULED"
-	 */
-	private $type;
+    /**
+     * @var string Enum:"ONECLICK" "UNSCHEDULED" "SCHEDULED"
+     */
+    private $type;
 
-	/**
-	 * @var string Enum: ECOM MOTO
-	 */
-	private $channel;
+    /**
+     * @var string Enum: ECOM MOTO
+     */
+    private $channel;
 
-	/**
-	 * @return string
-	 */
-	public function getType() {
-		return $this->type;
-	}
+    /**
+     * Date after which no further authorizations shall be performed
+     *
+     * @var string <date>
+     */
+    private $recurringExpiry;
 
-	/**
-	 * @param string $type
-	 */
-	public function setType($type) {
-		$this->type = $type;
-	}
+    /**
+     * Indicates the minimum number of days between authorizations [ 1 .. 9999 ]
+     *
+     * @var int
+     */
+    private $recurringFrequency;
 
-	/**
-	 * @return string
-	 */
-	public function getChannel() {
-		return $this->channel;
-	}
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
 
-	/**
-	 * @param string $channel
-	 */
-	public function setChannel($channel) {
-		$this->channel = $channel;
-	}
+    /**
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
 
+    /**
+     * @return string
+     */
+    public function getChannel()
+    {
+        return $this->channel;
+    }
+
+    /**
+     * @param string $channel
+     */
+    public function setChannel($channel)
+    {
+        $this->channel = $channel;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRecurringExpiry()
+    {
+        return $this->recurringExpiry;
+    }
+
+    /**
+     * @param string $recurringExpiry
+     */
+    public function setRecurringExpiry($recurringExpiry)
+    {
+        $this->recurringExpiry = $recurringExpiry;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRecurringFrequency()
+    {
+        return $this->recurringFrequency;
+    }
+
+    /**
+     * @param int $recurringFrequency
+     */
+    public function setRecurringFrequency($recurringFrequency)
+    {
+        $this->recurringFrequency = $recurringFrequency;
+    }
 }
